930250bef0e29d6730db31768e4951a35b3e5103
Md Ikbal <ikbal84336@gmail.com>
2026-01-04 22:13:14 +0530

    04 Header

---
+++ src/modules/home/components/navbar.jsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import {
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+
+import { cn } from "@/lib/utils";
+
+export const Navbar = () => {
+  return (
+    <nav
+      className={cn(
+        "p-4 bg-transparent fixed top-0 left-0 right-0 z-50 transition-all , duration-200 border-b border-transparent"
+      )}
+    >
+      <div className="max-w-5xl mx-auto w-full flex justify-between items-center">
+        <Link href={"/"} className="flex items-center gap-2">
+          <Image
+            src={"/logo.svg"}
+            alt="Vibe"
+            width={32}
+            height={32}
+            className="shrink-0 invert dark:invert-0"
+          />
+        </Link>
+        <div class="max-w-5xl mx-auto w-full flex flex-row-reverse justify-between items-center">
+          <UserButton />
+        </div>
+        <SignedOut>
+          <div className="flex gap-2">
+            <SignUpButton>
+              <Button variant={"outline"} size={"sm"}>
+                Sign In
+              </Button>
+            </SignUpButton>
+            <SignInButton>
+              <Button size={"sm"}>Sign Up</Button>
+            </SignInButton>
+          </div>
+        </SignedOut>
+      </div>
+    </nav>
+  );
+};
